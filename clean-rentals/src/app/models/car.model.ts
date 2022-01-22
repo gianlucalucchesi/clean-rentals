1582d47d30df432531c735cdf1bea862b81ad970
Gianluca Lucchesi <gianluca@Gianlucas-MacBook-Pro.local>
2022-01-22 21:51:57 +0100

    Added car and reservation options

--- clean-rentals/src/app/models/car.model.ts
+++ clean-rentals/src/app/models/car.model.ts
@@ -6,7 +6,7 @@ export class Car {
   brand: Brand;
   model: string;
   image_url: string;
-  carOptions?: CarOption;
+  carOptions?: CarOption[];
   start_day_price_euro_excl_vat: number;
   seats: number;
   doors: number;
