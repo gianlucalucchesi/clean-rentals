c9b825106a967a0270aa415706698e150ef5cd29
Gianluca Lucchesi <gianluca@Gianlucas-MacBook-Pro.local>
2022-01-29 12:16:37 +0100

    Added todo

--- api/src/main/java/com/cleanrentals/api/config/AppConfig.java
+++ api/src/main/java/com/cleanrentals/api/config/AppConfig.java
@@ -7,4 +7,5 @@ public class AppConfig {
     // TODO: check out CQRS https://www.baeldung.com/cqrs-event-sourcing-java
     // TODO: add retry policy wth Spring Retry https://www.baeldung.com/spring-retry
     // TODO: use interceptor https://www.baeldung.com/spring-rest-template-interceptor
+    // TODO: use HTTPS https://www.thomasvitale.com/https-spring-boot-ssl-certificate/
 }
