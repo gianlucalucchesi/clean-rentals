6d2d26782f1e35797bb29334d0ff74afa055fc2c
Gianluca Lucchesi <gianluca@Gianlucas-MacBook-Pro.local>
2022-01-29 16:34:14 +0100

    Updated url for HTTPS API

--- clean-rentals/src/environments/environment.ts
+++ clean-rentals/src/environments/environment.ts
@@ -1,19 +1,20 @@
 export const environment = {
   production: false,
-  ApiUrl: 'http://localhost:8080/api/', // used as reusable var in service
+  ApiUrl: 'https://localhost:8080/api/', // used as reusable var in service
   auth: {
     domain: 'dev-qkw53hzi.eu.auth0.com',
     clientId: 'zs22JkuRKqL7isbB4AkqWv75C0KvHmXB',
-    audience: 'http://localhost:8080/',
+    audience: 'https://localhost:8080/',
     redirectUri: window.location.origin,
   },
   httpInterceptor: {
     allowedList: [
-      'http://localhost:8080/api/*',
+      'https://localhost:8080/api/*',
       {
         tokenOptions: {
-          audience: 'http://localhost:8080',
-          scope: 'read:cars write:cars read:brands write:brands',
+          audience: 'https://localhost:8080',
+          //scope: 'read:cars write:cars read:brands write:brands',
+          scope: 'read: cars',
         },
       },
     ],
