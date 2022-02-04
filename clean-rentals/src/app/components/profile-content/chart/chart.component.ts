1fc4c79157e73e7ab0e8c5fae60ff67e65df0b7b
Gianluca Lucchesi <gianluca@Gianlucas-MacBook-Pro.local>
2022-02-04 11:08:51 +0100

    Fixed bug that brand names that were too long are not being displayed in the google chart

--- clean-rentals/src/app/components/profile-content/chart/chart.component.ts
+++ clean-rentals/src/app/components/profile-content/chart/chart.component.ts
@@ -84,7 +84,11 @@ export class ChartComponent implements OnInit, OnChanges {
         }
 
         // Concatenation as workaround for showing value next to label in chart
-        brandData.push(brand + ' (' + count + ')', count);
+        if(brand.length > 11) {
+          brandData.push(brand.substring(0, 8) + '...' + ' (' + count + ')', count)
+        } else {
+          brandData.push(brand + ' (' + count + ')', count);
+        }
         this.data.push(brandData);
       }
     }
