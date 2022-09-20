cc5960aba6a39705b21f26a62d9a5e3d42718270
Gianluca Lucchesi <gianluca@Gianlucas-MacBook-Pro.local>
2022-09-20 13:22:33 +0200

    added reservation url basepaths for running on home network or shared connection from iPhone

--- clean_rentals_mobile/lib/screens/reservations_overview_screen.dart
+++ clean_rentals_mobile/lib/screens/reservations_overview_screen.dart
@@ -1,6 +1,6 @@
-import 'package:clean_rentals_mobile/widgets/reservation_list.dart';
 import 'package:flutter/material.dart';
 
+import '../widgets/reservation_list.dart';
 import '../widgets/app_drawer.dart';
 import '../widgets/reservation_grid.dart';
 
--- clean_rentals_mobile/lib/services/reservation_service.dart
+++ clean_rentals_mobile/lib/services/reservation_service.dart
@@ -5,10 +5,15 @@ import 'package:http/http.dart' as http;
 import '../models/reservation.dart';
 
 class ReservationService {
-  static const getReservationsUrl = 'https://localhost:8080/api/v1/reservation';
+  static const getReservationsUrlLocal =
+      'https://localhost:8080/api/v1/reservation';
+  static const getReservationsUrlHome =
+      'https://192.168.0.162:8080/api/v1/reservation';
+  static const getReservationsUrlMobile =
+      'https://172.20.10.5:8080/api/v1/reservation';
 
   static Future<List<Reservation>> getReservations() async {
-    final response = await http.get(Uri.parse(getReservationsUrl));
+    final response = await http.get(Uri.parse(getReservationsUrlHome));
 
     if (response.statusCode == 200) {
       final jsonResponse = json.decode(response.body);
