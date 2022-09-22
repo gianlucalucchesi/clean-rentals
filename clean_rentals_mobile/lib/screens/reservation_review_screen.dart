d42e4ddf785c72f1a9761b1cd6a649b6f467ea3a
Gianluca Lucchesi <gianluca@Gianlucas-MacBook-Pro.local>
2022-09-22 22:15:20 +0200

    Keyboard now hides when tapping away form review text input

--- clean_rentals_mobile/lib/screens/reservation_review_screen.dart
+++ clean_rentals_mobile/lib/screens/reservation_review_screen.dart
@@ -27,54 +27,57 @@ class _ReservationReviewScreenState extends State<ReservationReviewScreen> {
   Widget build(BuildContext context) {
     final reservationId = ModalRoute.of(context)?.settings.arguments as String;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Review"),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              margin: const EdgeInsets.all(20.0),
-              child: Column(
-                children: [
-                  TextFormField(
-                    controller: reviewTextController,
-                    decoration: const InputDecoration(
-                      border: UnderlineInputBorder(),
-                      labelText: 'Findings',
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Review"),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: reviewTextController,
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Findings',
+                      ),
+                      maxLines: null,
+                      keyboardType: TextInputType.multiline,
                     ),
-                    maxLines: null,
-                    keyboardType: TextInputType.multiline,
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            const Divider(),
-            const Center(
-              child: ImageInput(),
-            ),
-            const Divider(),
-            const SizedBox(
-              height: 20,
-            ),
-            SizedBox(
-              height: 40,
-              width: 200,
-              child: ElevatedButton(
-                onPressed: () async {
-                  bool succes = await ReservationService.finalizeReservation(
-                      reservationId, reviewTextController.text);
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) =>
-                        _buildPopupDialog(context, succes),
-                  );
-                },
-                child: const Icon(Icons.check_circle_outline_sharp),
+              const Divider(),
+              const Center(
+                child: ImageInput(),
               ),
-            )
-          ],
+              const Divider(),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                height: 40,
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: () async {
+                    bool succes = await ReservationService.finalizeReservation(
+                        reservationId, reviewTextController.text);
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) =>
+                          _buildPopupDialog(context, succes),
+                    );
+                  },
+                  child: const Icon(Icons.check_circle_outline_sharp),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
